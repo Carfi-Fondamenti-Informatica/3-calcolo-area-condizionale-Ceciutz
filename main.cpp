68dfd068b48ee73dfcd9dc0524c5f58a4095ed21
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-11-21 08:11:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   float a=0,b=0;
+   int opzione=0;
+   cin>>a>>b>>opzione;
+   switch(opzione){
+      case 0:
+         cout<<((b*a)/2)<<endl;
+         break;
+      case 1:
+         cout<<(a*a)<<endl;
+         break;
+      case 2:
+         cout<<(a*b)<<endl;
+         break;
+      default:
+         cout<<"Opzione non valida"<<endl;
+   }     
    return 0;
 }
